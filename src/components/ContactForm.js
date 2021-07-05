bd9f4a064db90aa3c48ce50e10f481154d4ea519
Lee <lee.neufeld@ganjagurus.ca>
2021-07-05 10:25:52 -0600

    Style form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { init, send } from "emailjs-com";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 init("user_EJ7w4A5FKsK26PyN0Hy7i");
 
 const ContactForm = () => {
   const [toSend, setToSend] = useState({
     from_name: "",
-    to_name: "",
-    message: "",
     reply_to: "",
+    message: "",
   });
 
   const onSubmit = (e) => {
@@ -20,6 +20,7 @@ const ContactForm = () => {
     )
       .then((response) => {
         console.log("SUCCESS!", response.status, response.text);
+        alert("Your email has been sent!");
         window.location.reload();
       })
       .catch((err) => {
@@ -33,37 +34,37 @@ const ContactForm = () => {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="from_name"
-          placeholder="from name"
-          value={toSend.from_name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="to_name"
-          placeholder="to name"
-          value={toSend.to_name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="Your message"
-          value={toSend.message}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="reply_to"
-          placeholder="Your email"
-          value={toSend.reply_to}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Row className="pl-2 pr-2">
+          <Form.Control
+            className="mb-2"
+            type="text"
+            name="from_name"
+            placeholder="Name"
+            value={toSend.from_name}
+            onChange={handleChange}
+          />
+
+          <Form.Control
+            className="mb-2"
+            type="email"
+            name="reply_to"
+            placeholder="Your email"
+            value={toSend.reply_to}
+            onChange={handleChange}
+          />
+
+          <Form.Control
+            className="mb-2"
+            as="textarea"
+            name="message"
+            placeholder="Your message"
+            value={toSend.message}
+            onChange={handleChange}
+          />
+          <Button type="submit">Submit</Button>
+        </Row>
+      </Form>
     </div>
   );
 };
